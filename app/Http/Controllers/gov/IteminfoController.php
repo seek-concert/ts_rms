3e76664e945e2ad22c03a7e764c2029188bace6f
seek_concert <1105420247@qq.com>
2018-04-28 14:22:00 +0800

    张_征收管理_项目概括饼状图

--- app/Http/Controllers/gov/IteminfoController.php
+++ app/Http/Controllers/gov/IteminfoController.php
@@ -6,9 +6,11 @@
 */
 namespace App\Http\Controllers\gov;
 
+use App\Http\Model\Estate;
 use App\Http\Model\Filecate;
 use App\Http\Model\Filetable;
 use App\Http\Model\Household;
+use App\Http\Model\Householddetail;
 use App\Http\Model\Item;
 use App\Http\Model\Itemadmin;
 use Illuminate\Http\Request;
@@ -31,6 +33,7 @@ class IteminfoController extends BaseitemController
             if(blank($this->item)){
                 throw new \Exception('项目不存在',404404);
             }
+            //项目概述
             $itemadmins=Itemadmin::with(['user'=>function($query){
                 $query->select(['id','name']);
             }])
@@ -39,10 +42,62 @@ class IteminfoController extends BaseitemController
                 ->get();
             $household_num=Household::sharedLock()->where('item_id',$this->item_id)->count();
 
+            //摸底调查
+            $household = Household::sharedLock()
+                ->select([DB::raw('COUNT(*) as `survey_num`')])
+                ->where('item_id',$this->item_id)
+                ->first();
+            $household_detail = Householddetail::sharedLock()
+                ->select([DB::raw('count(*) as `survey_num`')])
+                ->where('item_id',$this->item_id)
+                ->first();
+            $survey_nums = [];
+            $survey_nums[0]['survey_num'] = $household['survey_num']-$household_detail['survey_num'];
+            $survey_nums[0]['name_info'] = '未调查';
+            $survey_nums[1]['survey_num'] = $household_detail['survey_num'];
+            $survey_nums[1]['name_info'] = '已调查';
+            //确权确户
+            $household_count = Household::sharedLock()
+                    ->select([DB::raw('count(*) as `household_count`')])
+                    ->whereBetween('code',[60,62])
+                    ->where('item_id',$this->item_id)
+                    ->first();
+            $household_nums = Household::sharedLock()
+                ->select([DB::raw('count(*) as `household_num`')])
+                ->where('code','>',62)
+                ->where('item_id',$this->item_id)
+                ->first();
+            $property_nums = [];
+            $property_nums[0]['property_num'] =$household_count['household_count'];
+            $property_nums[0]['name_info'] = '未确权';
+            $property_nums[1]['property_num'] = $household_nums['household_num'];
+            $property_nums[1]['name_info'] = '已确权';
+            //评估状况
+            $com_count = Estate::sharedLock()
+                ->with(['state'])
+                ->select([DB::raw('count(*) as `com_count`'),'code'])
+                ->whereBetween('code',[0,136])
+                ->where('item_id',$this->item_id)
+                ->groupBy('code')
+                ->get();
+            //协议签约
+            $contract_count = Household::sharedLock()
+                ->with(['state'])
+                ->select([DB::raw('count(*) as `contract_count`'),'code'])
+                ->whereBetween('code',[60,80])
+                ->where('item_id',$this->item_id)
+                ->groupBy('code')
+                ->get();
+
+
+            $data['survey_nums'] = $survey_nums;
+            $data['property_nums'] = $property_nums;
+            $data['com_count'] = $com_count;
+            $data['contract_count'] = $contract_count;
             $code='success';
             $msg='查询成功';
             $sdata=['item'=>$this->item,'itemadmins'=>$itemadmins,'household_num'=>$household_num];
-            $edata=null;
+            $edata=$data;
             $url=null;
 
             $view='gov.iteminfo.index';
@@ -56,7 +111,6 @@ class IteminfoController extends BaseitemController
             $view='gov.error';
         }
         DB::commit();
-
         /* ++++++++++ 结果 ++++++++++ */
         $result=['code'=>$code,'message'=>$msg,'sdata'=>$sdata,'edata'=>$edata,'url'=>$url];
         if($request->ajax()){
--- resources/views/gov/iteminfo/index.blade.php
+++ resources/views/gov/iteminfo/index.blade.php
@@ -98,6 +98,75 @@
                     </div>
                 </div>
             </div>
+
+            <div class="widget-container-col ui-sortable">
+                <div class="widget-box ui-sortable-handle">
+                    <div class="widget-header">
+                        <h5 class="widget-title">被征收户概括</h5>
+                    </div>
+
+                    <div class="widget-body">
+                        <div class="widget-main row">
+
+                            <div class="col-xs-6 col-sm-3 pricing-box">
+                                <div class="widget-box widget-color-orange">
+                                    <div class="widget-header">
+                                        <h5 class="widget-title bigger lighter">摸底调查状态</h5>
+                                    </div>
+
+                                    <div class="widget-body">
+                                        <div class="widget-main" style="min-width: 300px;min-height: 300px" id="survey_nums_household">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-6 col-sm-3 pricing-box">
+                                <div class="widget-box widget-color-blue">
+                                    <div class="widget-header">
+                                        <h5 class="widget-title bigger lighter">确权确户状态</h5>
+                                    </div>
+
+                                    <div class="widget-body">
+                                        <div class="widget-main" style="min-width: 300px;min-height: 300px" id="property_household">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-6 col-sm-3 pricing-box">
+                                <div class="widget-box widget-color-green">
+                                    <div class="widget-header">
+                                        <h5 class="widget-title bigger lighter">房产评估状况</h5>
+                                    </div>
+
+                                    <div class="widget-body">
+                                        <div class="widget-main" style="min-width: 300px;min-height: 300px" id="com_household">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-6 col-sm-3 pricing-box">
+                                <div class="widget-box widget-color-grey">
+                                    <div class="widget-header">
+                                        <h5 class="widget-title bigger lighter">协议签约比例</h5>
+                                    </div>
+
+                                    <div class="widget-body">
+                                        <div class="widget-main" style="min-width: 300px;min-height: 300px" id="contract_household">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -115,10 +184,133 @@
 @section('js')
     @parent
     <script src="{{asset('viewer/viewer.min.js')}}"></script>
-
+    <script src="{{asset('echarts/echarts.common.min.js')}}"></script>
     <script src="{{asset('js/func.js')}}"></script>
     <script>
-        $('.img-content').viewer();
+        $('.img-content').viewer('update');
+        @if($code=='success')
+        // 摸底调查
+        var survey_nums_household=@json($edata['survey_nums']);
+        items=[];
+        values=[];
+        $.each(survey_nums_household,function (index,info) {
+            items.push((info.name_info|| '其他'));
+            values.push({value:info.survey_num,name:(info.name_info|| '其他')});
+        });
+        echarts.init(document.getElementById('survey_nums_household')).setOption({
+            tooltip : {
+                trigger: 'item',
+                formatter: "{b}：<br/>{c} 户 <br/>({d}%)"
+            },
+            series : [
+                {
+                    name: null,
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:values,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        });
+        //确权确户
+        var property_household=@json($edata['property_nums']);
+        items=[];
+        values=[];
+        $.each(property_household,function (index,info) {
+            items.push((info.name_info|| '其他'));
+            values.push({value:info.property_num,name:(info.name_info|| '其他')});
+        });
+        echarts.init(document.getElementById('property_household')).setOption({
+            tooltip : {
+                trigger: 'item',
+                formatter: "{b}：<br/>{c} 户 <br/>({d}%)"
+            },
+            series : [
+                {
+                    name: null,
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:values,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        });
+        //评估状况
+        var com_household=@json($edata['com_count']);
+        items=[];
+        values=[];
+        $.each(com_household,function (index,info) {
+            items.push((info.state.name|| '其他'));
+            values.push({value:info.com_count,name:(info.state.name|| '其他')});
+        });
+        echarts.init(document.getElementById('com_household')).setOption({
+            tooltip : {
+                trigger: 'item',
+                formatter: "{b}：<br/>{c} 户 <br/>({d}%)"
+            },
+            series : [
+                {
+                    name: null,
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['55%', '60%'],
+                    data:values,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        });
+        //协议签约
+        var contract_household=@json($edata['contract_count']);
+        items=[];
+        values=[];
+        $.each(contract_household,function (index,info) {
+            items.push((info.state.name|| '其他'));
+            values.push({value:info.contract_count,name:(info.state.name|| '其他')});
+        });
+        echarts.init(document.getElementById('contract_household')).setOption({
+            tooltip : {
+                trigger: 'item',
+                formatter: "{b}：<br/>{c} 户 <br/>({d}%)"
+            },
+            series : [
+                {
+                    name: null,
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['55%', '60%'],
+                    data:values,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        });
+        @endif
+
     </script>
 
 @endsection
